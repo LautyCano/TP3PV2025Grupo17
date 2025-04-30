bf8917213d5708ee1563d28bd96fbf4b781a4421
Dani444-S <tiagosegobia444@gmail.com>
2025-04-30 03:41:35 -0300

    Terminar de resolver el formulario de la lista de tareas To Do (nombre, descripcion, fecha) con useState, Segobia Tiago

--- src/assets/components/Tarea.jsx
+++ src/assets/components/Tarea.jsx
@@ -1,28 +1,63 @@
 import '/src/assets/css/tarea.css';
+import { useState } from "react";
+
+
 
 function Tarea() {
-    return (
-        <>
-            <div className='Titulo'>
-            <h1>Listado de tareas</h1>
-        </div>
 
-        <div>
-            <form>
-                <label htmlFor="nombre">Nombre de la Tarea:</label>
-                <input type="text" id="nombre" name="nombre" required />
+    const [nombre, setNombre] = useState("");
+    const [descripcion, setDescripcion] = useState("");
+    const [fecha, setFecha] = useState("");
+    const [tareas, setTareas] = useState([]);
 
-                <label htmlFor="descripcion">Descripción:</label>
-                <textarea id="descripcion" name="descripcion" rows="4" required></textarea>
+    const manejarEnvio = (e) => {
 
-                <label htmlFor="fecha">Fecha límite:</label>
-                <input type="date" id="fecha" name="fecha" required />
+        if (nombre && descripcion && fecha) {
+        const nuevaTarea = {
+            nombre,
+            descripcion,
+            fecha
+        };
 
-                <button type="submit">Guardar Tarea</button>
-            </form>
-        </div>
-        </>
-    );
+        setTareas([...tareas, nuevaTarea]);
+        console.log("Lista de tareas actualizada:",[...tareas, nuevaTarea]);
+
+        setNombre("");
+        setDescripcion("");
+        setFecha("");
+        } 
+        else {
+            alert("Por favor, completa todos los campos.");
+        }
+    };
+        return (
+            <>
+                <div className='Titulo'>
+                    <h1>Listado de tareas</h1>
+                </div>
+
+                <div>
+                    <form>
+                        <label htmlFor="nombre">Nombre de la Tarea:</label>
+                        <input type="text" id="nombre" name="nombre" required
+                        value={nombre}
+                        onChange={(e) => setNombre(e.target.value)} />
+
+                        <label htmlFor="descripcion">Descripción:</label>
+                        <textarea id="descripcion" name="descripcion" rows="4" required
+                        value={descripcion}
+                        onChange={(e) => setDescripcion(e.target.value)}></textarea>
+
+                        <label htmlFor="fecha">Fecha límite:</label>
+                        <input type="date" id="fecha" name="fecha" required
+                        value={fecha}
+                        onChange={(e) => setFecha(e.target.value)}/>
+
+                        <button type="button" onClick={manejarEnvio}>Guardar Tarea</button>
+                    </form>
+                </div>
+            </>
+        );
+
 }
-
 export default Tarea
